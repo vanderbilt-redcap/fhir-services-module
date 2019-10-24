de4d142e1cc04cf692c4be25bd7c5ef531a38c26
Mark McEver <mark@fastmail.net>
2019-10-24 16:10:31 +0100

    Renamed a method

--- FHIRUtil.php
+++ FHIRUtil.php
@@ -164,7 +164,7 @@ class FHIRUtil
         $out = fopen('php://memory', 'r+');
         fputcsv($out, ["Variable / Field Name","Form Name","Section Header","Field Type","Field Label","Choices, Calculations, OR Slider Labels","Field Note","Text Validation Type OR Show Slider Number","Text Validation Min","Text Validation Max","Identifier?","Branching Logic (Show field only if...)","Required Field?","Custom Alignment","Question Number (surveys only)","Matrix Group Name","Matrix Ranking?","Field Annotation"]);
        
-        self::walkFHIRObject($q, function($parent, $item) use ($out){
+        self::walkQuestionnaire($q, function($parent, $item) use ($out){
             $instrumentName = self::getInstrumentName($parent);
 
             // We'll use the instrument name function for fields too until UAMS comes up with better field names.
@@ -178,7 +178,7 @@ class FHIRUtil
         return stream_get_contents($out);
     }
 
-    function walkFHIRObject($group, $fieldAction){
+   function walkQuestionnaire($group, $fieldAction){
         $handleItems = function ($group) use (&$handleItems, &$out, &$fieldAction){
             $groupId = self::getLinkId($group);
 
