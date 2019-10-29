dd1fb1dbc1eff78cb2d42205449052edc51f46c6
Mark McEver <mark@fastmail.net>
2019-10-29 18:12:51 +0100

    Only showed the bundle button on the compositions project

--- FHIRServicesExternalModule.php
+++ FHIRServicesExternalModule.php
@@ -2,22 +2,24 @@
 
 class FHIRServicesExternalModule extends \ExternalModules\AbstractExternalModule{
     function redcap_data_entry_form($project_id, $record){
-        ?>
-        <script>
-            (function(){
-                var pdfButton = $('#pdfExportDropdownTrigger')
-                var bundleButton = $('<a href="<?=$this->getUrl('service.php', true) . "&fhir-url=/Composition/$record/\$document"?>">Create FHIR Bundle</a>')
-                bundleButton.attr('class', pdfButton.attr('class'))
-                bundleButton.css({
-                    'margin-left': '3px',
-                    'min-height': '26px',
-                    'margin-bottom': '15px',
-                    'vertical-align': 'top'
-                })
-
-                pdfButton.after(bundleButton)
-            })()
-        </script>
-        <?php
+        if(\REDCap::getRecordIdField() === 'composition_id'){
+            ?>
+            <script>
+                (function(){
+                    var pdfButton = $('#pdfExportDropdownTrigger')
+                    var bundleButton = $('<a href="<?=$this->getUrl('service.php', true) . "&fhir-url=/Composition/$record/\$document"?>">Create FHIR Bundle</a>')
+                    bundleButton.attr('class', pdfButton.attr('class'))
+                    bundleButton.css({
+                        'margin-left': '3px',
+                        'min-height': '26px',
+                        'margin-bottom': '15px',
+                        'vertical-align': 'top'
+                    })
+    
+                    pdfButton.after(bundleButton)
+                })()
+            </script>
+            <?php
+        }
     }
 }
