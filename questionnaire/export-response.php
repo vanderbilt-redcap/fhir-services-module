5606aa91800c31d17f3aa4df2d62cfe4929b087a
Mark McEver <mark@fastmail.net>
2019-11-07 20:42:47 +0000

    Implemented basic questionnaire response generation

--- questionnaire/export-response.php
+++ questionnaire/export-response.php
@@ -1,31 +1,55 @@
 <?php
 
 use DCarbone\PHPFHIRGenerated\R4\FHIRResource\FHIRDomainResource\FHIRQuestionnaireResponse;
+use DCarbone\PHPFHIRGenerated\R4\FHIRElement\FHIRBackboneElement\FHIRQuestionnaireResponse\FHIRQuestionnaireResponseAnswer;
 use DCarbone\PHPFHIRGenerated\R4\FHIRElement\FHIRBackboneElement\FHIRQuestionnaireResponse\FHIRQuestionnaireResponseItem;
 
 $data = $module->getData($module->getProjectId(), $_GET['id'])[0];
 
 $edoc = $module->getQuestionnaireEDoc();
 
-$q = $module->parse(file_get_contents(EDOC_PATH . $edoc['stored_name']));
+$questionnaire = $module->parse(file_get_contents(EDOC_PATH . $edoc['stored_name']));
 
-$objects = [];
-$module->walkQuestionnaire($q, function($parents, $item) use ($module, $data){
+$questionnaireResponse = new FHIRQuestionnaireResponse;
+$responseObjects = [];
+$getResponseObject = function($parentResponseItem, $item) use ($module, &$responseObjects, $questionnaireResponse){
+    if(!$parentResponseItem){
+        // This is the Questionnaire root
+        return $questionnaireResponse;
+    }
+
+    $itemId = $module->getLinkId($item);
+    $responseItem = $responseObjects[$itemId];
+    if(!$responseItem){      
+        $responseItem = new FHIRQuestionnaireResponseItem;
+        $responseItem->setLinkId($itemId);
+        $responseItem->setText($item->getText());
+
+        $responseObjects[$itemId] = $responseItem;
+        $parentResponseItem->addItem($responseItem);
+    }
+
+    return $responseItem;
+};
+
+$module->walkQuestionnaire($questionnaire, function($parents, $item) use ($module, $data, &$getResponseObject){
     $fieldName = $module->getFieldName($item);
     $value = $data[$fieldName];
     if(!$value){
         return;
     }
 
-    $linkId = $item->getLinkId();
-    if(isset($objects[$linkId])){
-        throw new Exception("LinkId set multiple times: $linkId");
-    }
+    $items = array_merge($parents, [$item]);
+    $lastResponseItem = null; 
+    foreach($items as $item){
+        $responseItem = $getResponseObject($lastResponseItem, $item);
+        $lastResponseItem = $responseItem;
+    }    
 
-    $objects = new FHIRQuestionnaireResponseItem([
-        'value' => $value
-    ]);
+    $responseItem->addAnswer(new FHIRQuestionnaireResponseAnswer([
+        'valueString' => $value
+    ]));
 });
 
-// header("Content-Disposition: attachment; filename=questionnaire-export.json");
-// TODO - test/handle repeatables
+header("Content-Disposition: attachment; filename=questionnaire-export.json");
+echo $module->jsonSerialize($questionnaireResponse);
