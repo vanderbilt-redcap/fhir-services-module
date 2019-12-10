19c035562cc610b996a846626125674194a3d2d8
Mark McEver <mark@fastmail.net>
2019-12-10 17:11:15 -0600

    Added an IREx logo

--- templates/title.php
+++ templates/title.php
@@ -1,6 +1,6 @@
 <div xmlns="http://www.w3.org/1999/xhtml">
-    <p>
-        <img src="<?=$irexLogoUrl?>" alt="IREX"/>
+    <p style="text-align: center;">
+        <img src="<?=$irexLogoUrl?>" alt="IREX" style="width: 385px;"/>
     </p>
     <p style="text-align:center;text-decoration: underline;"> Official Documentation of Reliance</p>
     <p>Nov 19, 2018</p>
