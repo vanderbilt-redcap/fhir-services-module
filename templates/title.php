856f2e983e35532759c62e9f49ebb2cd87527a13
Mark McEver <mark@fastmail.net>
2019-12-12 15:18:14 +0000

    Updated determination letter date

--- templates/title.php
+++ templates/title.php
@@ -3,5 +3,5 @@
         <img src="<?=$irexLogoUrl?>" alt="IREX" style="width: 385px;"/>
     </p>
     <p style="text-align:center;text-decoration: underline;"> Official Documentation of Reliance</p>
-    <p>Nov 19, 2018</p>
+    <p><?=date('M j, Y')?></p>
 </div>
