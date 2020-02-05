9d254095659c474a0ee643dc8fa8dd083550c9ec
Mark McEver <mark@fastmail.net>
2020-02-05 20:18:49 +0000

    Unit tested a REDCap method I wanted to use

--- tests/FHIRServicesExternalModuleTest.php
+++ tests/FHIRServicesExternalModuleTest.php
@@ -78,4 +78,22 @@ class FHIRServicesExternalModuleTest extends \ExternalModules\ModuleBaseTest{
         $assert(true);
         $assert(false);
     }
+
+    function testGetValueInActionTag(){
+        // This method exists in REDCap core, but I wanted to test it just to make sure it worked in all the cases I expected it to.
+        
+        $paramName = 'FOO';
+        
+        $assert = function($expected, $actionTags) use ($paramName){
+            $actual = \Form::getValueInActionTag($actionTags, "@$paramName");
+            $this->assertSame($expected, $actual);
+        };
+
+        $assert('1', "@$paramName=1");
+        $assert('2', "@$paramName=2 @SOME-OTHER-TAG"); // first of multiple
+        $assert('3', "@SOME-TAG @$paramName=3"); // last of multiple
+        $assert('4', "@SOME-TAG @$paramName=4 @SOME-OTHER-TAG"); // middle of mutliple
+        $assert('5 6', "@$paramName='5 6'");
+        $assert('7 8', "@$paramName=\"7 8\"");
+    }
 }
