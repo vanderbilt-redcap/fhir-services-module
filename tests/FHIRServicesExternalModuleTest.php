05235476cefcab3f7fca8e9b037b02668a1c504f
Mark McEver <mark@fastmail.net>
2020-02-05 19:35:00 +0000

    Renamed a class

--- tests/FHIRServicesExternalModuleTest.php
+++ tests/FHIRServicesExternalModuleTest.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../../../redcap_connect.php';
 
 use DCarbone\PHPFHIRGenerated\R4\FHIRElement\FHIRBackboneElement\FHIRQuestionnaire\FHIRQuestionnaireItem;
 
-class QuestionnaireExportTest extends \ExternalModules\ModuleBaseTest{
+class FHIRServicesExternalModuleTest extends \ExternalModules\ModuleBaseTest{
     function testCreateQuestionnaire(){
         $fields = json_decode(file_get_contents(__DIR__ . '/fields.json'), true);
         list($questionnaire, $skippedFields) = $this->createQuestionnaire(116, 'all_field_type_examples', 'All Field Type Examples', $fields, []);
