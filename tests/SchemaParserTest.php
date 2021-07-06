846264c7237e12e99b6ce821f19c2ed19d6a6ad9
Mark McEver <mark@fastmail.net>
2021-07-06 21:01:46 +0100

    Improved SchemaParserTest output

--- tests/SchemaParserTest.php
+++ tests/SchemaParserTest.php
@@ -11,22 +11,36 @@ class SchemaParserTest extends BaseTest{
             }
         }
 
-        $handleError = function($message) use ($actual){    
-            $actualPath = __DIR__ . '/actual-modified-schema-summary.json';
-            file_put_contents($actualPath, json_encode($actual, JSON_PRETTY_PRINT));
-            throw new Exception($message);
+        $assertDiff = function($one, $two, $message) use ($actual){
+            $diffs = [];
+            foreach($one as $resourceName => $onePaths){
+                $twoPaths = $two[$resourceName];
+                $diff = array_diff($onePaths, $twoPaths);
+                if(!empty($diff)){
+                    $diffs[$resourceName] = $diff;
+                }
+            }
+    
+            if(!empty($diffs)){
+                $actualPath = __DIR__ . '/actual-modified-schema-summary.json';
+                file_put_contents($actualPath, json_encode($actual, JSON_PRETTY_PRINT));
+                
+                foreach($diffs as $resourceName => $elementPaths){
+                    foreach($elementPaths as $elementPath){
+                        // Create lines for each grepping to verify changes.
+                        echo "modified-schema-diff: $resourceName/$elementPath\n";
+                    }
+                }
+
+                throw new Exception($message);
+            }
         };
 
         $expected = json_decode(file_get_contents(__DIR__ . '/expected-modified-schema-summary.json'), true);
         if($actual !== $expected){
-            foreach($expected as $resourceName => $expectedPaths){
-                $actualPaths = $actual[$resourceName];
-                if(!empty(array_diff($expectedPaths, $actualPaths))){
-                    $handleError("Modified schema paths were removed!  Please review the difference between the expected & actual modified schema summary files.");
-                }
-            }
+            $assertDiff($expected, $actual, "The previously displayed modified schema paths were removed!  Please review the difference between the expected & actual modified schema summary files.");
 
-            $handleError("Modified schema paths were added!  If this was intentional, you may want to update the expected modified scheme summary with the actual summary.");
+            $assertDiff($actual, $expected, "The previously displayed modified schema paths were added!  If this was intentional, you may want to update the expected modified scheme summary with the actual summary.");
         }
 
         // Every test needs an assertion, even though the equals check above should already cover this.
