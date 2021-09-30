0fd18d831c1bf25344a4b7c4d0b81631b58f4237
Mark McEver <mark@fastmail.net>
2021-09-30 18:57:47 -0500

    Added support for positiveInt

--- FHIRServicesExternalModule.php
+++ FHIRServicesExternalModule.php
@@ -55,6 +55,7 @@ const ACTION_TAG_SUFFIX = "'";
 const SINGLE_QUOTE_PLACEHOLDER = '<single-quote-placeholder>';
 
 const INTEGER_PATTERN = "^-?([0]|([1-9][0-9]*))$";
+const POSITIVE_INT_PATTERN = "^[1-9][0-9]*$";
 const DATE_TIME_PATTERN = "^([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\\.[0-9]+)?(Z|(\\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?$";
 
 if (!function_exists('str_contains')) {
